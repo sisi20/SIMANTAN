f44ae4d97beda55cf06182c9f49db8bf936c6803
tohadibrex <toha@sgbteam.id>
2021-07-04 17:16:48 +0700

    Add files via upload

--- application/views/kegiatan/vw_kegiatan.php
+++ application/views/kegiatan/vw_kegiatan.php
@@ -1,4 +1,4 @@
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-6"><a href="<?= base_url(); ?>kegiatan/tambah" class="btn btn-info mb-2">Tambah Mahasiswa</a></div>
+        <div class="col-md-6"><a href="<?= base_url(); ?>kegiatan/tambah" class="btn btn-info mb-2">Tambah Kegiatan</a></div>
 </div>
