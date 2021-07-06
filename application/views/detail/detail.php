abb496a136d54d2bdead53e34b9a6674e3ccc96c
nurwulansetiani <72968126+nurwulansetiani@users.noreply.github.com>
2021-07-07 00:21:33 +0700

    Update detail.php

--- application/views/detail/detail.php
+++ application/views/detail/detail.php
@@ -1,28 +1,71 @@
 <div class="container">
     <div class="row">
-        <div class="col-md-6"><a href="<?= base_url(); ?>kegiatan/tambah" class="btn btn-info mb-2">Tambah Mahasiswa</a></div>
-
         <table class="table table-stripped">
+            <div class="col-md-6">
+                <p><small><?= $user->nama; ?></small> | <a href="<?= base_url('login/logout'); ?>"><small style="color: red;">Logout</small></a></p>
+            </div>
             <thead>
-                <th scope="col">Nama Kegiatan</th>
-                <th scope="col">Prioritas</th>
-                <th scope="col">PIC</th>
-                <th scope="col">Waktu</th>
-                <th scope="col">Status</th>
-            </thead>
-
-            <tbody>
                 <tr>
-                    <td><?= $kegiatan['kegiatan'] ?></td>
-                    <td><?= $kegiatan['prioritas'] ?></td>
-                    <td><?= $kegiatan['penanggung_jawab'] ?></td>
-                    <td><?= $kegiatan['waktu'] ?></td>
-                    <td><?php if ($kegiatan['status'] == 1) {
-                            echo 'Approved';
-                        } else {
-                            echo 'Menunggu';
-                        } ?></td>
+                <th scope="col">Nama Kegiatan</th>
+                <td><?= $kegiatan['kegiatan'] ?></td>
                 </tr>
-            </tbody>
+                <tr>
+                <th scope="col">Prioritas</th>
+                <td><?= $kegiatan['prioritas'] ?></td>
+                </tr>
+                <tr>
+                <th scope="col">PIC</th>
+                <td><?= $kegiatan['user'] ?></td>
+                </tr>
+                <tr>
+                <?=$timestamp = strtotime($kegiatan['waktu']);
+	        	$day = date('l', $timestamp);;
+		        if($day == 'Monday'){
+		    	$day = 'Senin';
+		        }else if($day == 'Tuesday'){
+			    $day = 'Selasa';
+		        }else if($day == 'Wednesday'){
+		        $day = 'Rabu';
+		        }else if($day == 'Thursday'){
+		    	$day = 'Kamis';
+		        }else if($day == 'Friday'){
+		    	$day = 'Jumat';
+	        	}else if($day == 'Saturday'){
+		    	$day = 'Sabtu';
+		        }else if($day == 'Sunday'){
+		        $day = 'Minggu';
+		        } ?>
+                <th scope="col">Waktu</th>
+                <td><?= $day?>, <?= $kegiatan['waktu'] ?></td>
+                </tr>
+                <tr>
+                <th scope="col">Tempat</th>
+                <td><?= $kegiatan['tempat'] ?></td>
+                </tr>
+                <tr>
+                <th scope="col">Lokasi</th>
+                <td><?= $kegiatan['lokasi'] ?></td>
+                </tr>
+                <tr>
+                <th scope="col">Skema</th>
+                <td>
+                <?= $kegiatan['skema_proses_masuk_keluar'] ?><br>
+                <?= $kegiatan['skema_penerapan_prokes'] ?><br>
+                <?= $kegiatan['skema_kegiatan_berlangsung'] ?><br>
+                <?= $kegiatan['skema_kegiatan_selesai'] ?>
+                </td>
+                </tr>
+                <tr>
+                <th scope="col">Status</th>
+                <td>
+                        <?php if ($kegiatan['status'] == 1) { ?>
+                         <p class="btn btn-success">Approved</p>
+                         <?php } else { ?>
+                        <p class="btn btn-warning">Menunggu</p>
+                        <?php } ?>
+              </td>
+                </tr>
+            </thead>
         </table>
     </div>
+    <div class="col-md-6"><a href="<?= base_url('detail/cetak/') . $kegiatan['id'] ?>" class="btn btn-info">cetak</a></div><br>
