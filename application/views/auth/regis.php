56d5c063738679ad77d293ed295227071f5ae8c6
sisi20 <53111167+sisi20@users.noreply.github.com>
2021-07-07 14:06:09 +0700

    Update regis.php

--- application/views/auth/regis.php
+++ application/views/auth/regis.php
@@ -10,9 +10,9 @@
                     <div class="text-center">
                         <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                     </div>
-                    <form class="user" method="post" action="<?php echo base_url('index.php/login/registration'); ?>">
+                    <form class="user" method="post" action="<?php echo base_url('login/proses_register'); ?>">
                         <div class="form-group">
-                            <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="username" placeholder="Username">
+                            <input type="email" class="form-control form-control-user" id="exampleInputEmail" name="email" placeholder="email">
                         </div>
                         <div class="form-group">
                             <input type="text" class="form-control form-control-user" id="exampleInputEmail" name="nama" placeholder="Name Institut">
@@ -21,26 +21,11 @@
                             <input type="password" class="form-control form-control-user" id="exampleInputEmail" name="password" placeholder="Password">
                         </div>
 
-                        <div class="form-group row">
-                            <div class="custom-control custom-checkbox small">
-                                &nbsp;&nbsp;&nbsp;&nbsp;<input type="checkbox" id="vehicle1" name="role" value="1">
-                                <label for="vehicle1"> Satgas</label><br>
-                            </div>
-                            <div class="custom-control custom-checkbox small">
-                                <input type="checkbox" id="role" name="role" value="2">
-                                <label for="vehicle2"> Staff</label><br>
-
-                            </div>
-                        </div>
-
+                    
                         <button type="submit" class="btn btn-primary btn-user btn-block">
                             Register Akun
                         </button>
                         <hr>
-                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                            <i class="fab fa-google fa-fw"></i> Register with Google
-                        </a>
-
                     </form>
                     <hr>
 
