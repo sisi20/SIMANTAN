7ef17311f01f2f84aec5d75f27b75c5eb366e0ba
sisi20 <53111167+sisi20@users.noreply.github.com>
2021-07-08 09:18:19 +0700

    Update regis.php

--- application/views/auth/regis.php
+++ application/views/auth/regis.php
@@ -7,9 +7,10 @@
                 <!-- Nested Row within Card Body -->
 
                 <div class="p-5">
-                    <div class="text-center">
-                        <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
-                    </div>
+                                    <div class="text-center">
+                                    <img src="<?php echo base_url()?>assets1/img/SiM@nTan-PCR.png"width="250" height="90" >
+                                    </img>
+                                </div>
                     <form class="user" method="post" action="<?php echo base_url('login/proses_register'); ?>">
                         <div class="form-group">
                             <input type="email" class="form-control form-control-user" id="exampleInputEmail" name="email" placeholder="email">
